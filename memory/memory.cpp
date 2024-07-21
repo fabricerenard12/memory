47bebd542db1e73ca918cf49357c4ebf8457c45b
Fabrice Renard <fabrice.renard12@outlook.com>
2024-07-21 01:45:21 -0400

    Add Memory Constructors

--- memory/memory.cpp
+++ memory/memory.cpp
@@ -6,6 +6,18 @@ Memory::Memory(HANDLE hProcess) : hProcess(hProcess) {}
 
 Memory::Memory(DWORD processID) : hProcess(OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID)) {}
 
+Memory::Memory(const wchar_t* processName) {
+    AttachProcess(processName);
+}
+
+Memory::Memory(const char* processName) {
+    AttachProcess(processName);
+}
+
+Memory::Memory(const std::string& processName) {
+    AttachProcess(processName);
+}
+
 Memory::~Memory() {
 	DetachProcess();
 }
@@ -23,6 +35,32 @@ bool Memory::AttachProcess(const wchar_t* processName) {
 
 	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
 	return hProcess != nullptr;
+}
+
+bool Memory::AttachProcess(const wchar_t* processName) {
+    DWORD processID = GetProcessID(processName);
+    if (processID == 0) return false;
+
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+    return hProcess != nullptr;
+}
+
+bool Memory::AttachProcess(const char* processName) {
+    const wchar_t* wideStrProcessName = CharToWchar(processName);
+    DWORD processID = GetProcessID(wideStrProcessName);
+    if (processID == 0) return false;
+
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+    return hProcess != nullptr;
+}
+
+bool Memory::AttachProcess(const std::string& processName) {
+    const wchar_t* wideStrProcessName = StringToWchar(processName);
+    DWORD processID = GetProcessID(wideStrProcessName);
+    if (processID == 0) return false;
+
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+    return hProcess != nullptr;
 }
 
 DWORD Memory::GetProcessID(const wchar_t* processName) {
@@ -44,6 +82,32 @@ DWORD Memory::GetProcessID(const wchar_t* processName) {
     }
 
     return processID;
+}
+
+const wchar_t* Memory::CharToWchar(const char* charArray) {
+    size_t len = std::mbstowcs(nullptr, charArray, 0);
+    if (len == static_cast<size_t>(-1)) {
+        return nullptr;
+    }
+
+    wchar_t* wcharArray = new wchar_t[len + 1];
+    std::mbstowcs(wcharArray, charArray, len);
+    wcharArray[len] = L'\0';
+
+    return wcharArray;
+}
+
+const wchar_t* Memory::StringToWchar(const std::string& str) {
+    size_t len = std::mbstowcs(nullptr, str.c_str(), 0);
+    if (len == static_cast<size_t>(-1)) {
+        return nullptr;
+    }
+
+    wchar_t* wcharArray = new wchar_t[len + 1];
+    std::mbstowcs(wcharArray, str.c_str(), len);
+    wcharArray[len] = L'\0';
+
+    return wcharArray;
 }
 
 bool Memory::ReadMemory(DWORD address, void* buffer, SIZE_T size) {
--- memory/memory.hpp
+++ memory/memory.hpp
@@ -1,17 +1,25 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#include <windows.h>
-#include <tlhelp32.h>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <cwchar>
+#include <cstdlib>
+#include <string>
 
 class Memory {
 public:
     Memory();
     Memory(HANDLE hProcess);
     Memory(DWORD processID);
+    Memory(const wchar_t* processName);
+    Memory(const char* processName);
+    Memory(const std::string& processName);
     ~Memory();
 
     bool AttachProcess(const wchar_t* processName);
+    bool AttachProcess(const char* processName);
+    bool AttachProcess(const std::string& processName);
     void DetachProcess();
     bool ReadMemory(DWORD address, void* buffer, SIZE_T size);
     bool WriteMemory(DWORD address, const void* buffer, SIZE_T size);
@@ -19,6 +27,8 @@ public:
 private:
     HANDLE hProcess;
     DWORD GetProcessID(const wchar_t* processName);
+    const wchar_t* CharToWchar(const char* charArray);
+    const wchar_t* StringToWchar(const std::string& str);
 };
 
 #endif // !MEMORY_H
